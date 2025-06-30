8644394ca3419b13b1c9ce3a16a5c11984f305ca
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-06-30 19:41:15 +0300

    Initial commit

---
+++ src/main/java/com/yapai/akademikaracom/AkademikaracomApplication.java
@@ -0,0 +1,13 @@
+package com.yapai.akademikaracom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkademikaracomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkademikaracomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yapai/akademikaracom/AkademikaracomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapai.akademikaracom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkademikaracomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
