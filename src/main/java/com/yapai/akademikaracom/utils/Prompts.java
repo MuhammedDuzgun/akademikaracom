b11a44c965253a820eb49c2159384b7fdfb285d5
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-07-05 04:21:14 +0300

    add new prompts

--- src/main/java/com/yapai/akademikaracom/utils/Prompts.java
+++ src/main/java/com/yapai/akademikaracom/utils/Prompts.java
@@ -1,13 +1,28 @@
 package com.yapai.akademikaracom.utils;
 
 public class Prompts {
-    public static final String articlePrompt = """
+    public static final String GET_SIMILAR_ARTICLES = """
             Kullanıcıdan gelen parametrelere dayanarak (makalenin başlığı, makalenin özeti)
             benzer en önemli bilimsel makaleleri sıralayan bir asistansın 
             """;
 
-    public static final String articlePrompt2 = """
+    public static final String ARTICLES_SOURCES = """
             Kullanıcıdan gelen paramtrelere dayanarak (makalenin başlığı)
-            o makaleyi tespit edip o makalede yer alan kaynakları IEEEE formatında listeleyen bir asistansın
+            o makaleyi tespit edip o makalede yer alan tüm kaynakları kullanıcının istediği formatta listeleyen bir asistansın
             """;
+    public static final String GET_QUOTATION_OF_ARTICLE = """
+            Kullanıcıdan gelen parametrelere dayanarak (makalenin başlığı)
+            o makaleyi tespit edip o makalenin alıntılandığı en önemli makaleleri listeleyen bir asistansın
+            """;
+
+    public static final String GET_ABSTRACT_OF_ARTICLE = """
+            Kullanıcıdan gelen parametrelere dayanarak (makalenin başlığı) 
+            o makaleyi tespit edip, o makalenin iyi ve kötü yanlarını şu özellikler çerçevesinde 
+            listeleyen bir asistansın: 
+            * türkçe dilinde
+            * makalenin özet ve sonuç bölümünden yararlanarak
+            * makaleye yapılan alıntıları dikkate alarak
+            * hem teknik hem de anlamsal açıdan
+            """;
+
 }
