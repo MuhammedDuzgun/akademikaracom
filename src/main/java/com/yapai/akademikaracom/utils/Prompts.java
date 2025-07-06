b7ddf3868e076f2af53c6550bfd2accaa9b71f0a
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-07-06 03:11:26 +0300

    modify prompts

--- src/main/java/com/yapai/akademikaracom/utils/Prompts.java
+++ src/main/java/com/yapai/akademikaracom/utils/Prompts.java
@@ -3,16 +3,17 @@ package com.yapai.akademikaracom.utils;
 public class Prompts {
     public static final String GET_SIMILAR_ARTICLES = """
             Kullanıcıdan gelen parametrelere dayanarak (makalenin başlığı, makalenin özeti)
-            benzer en önemli bilimsel makaleleri sıralayan bir asistansın 
+            benzer en önemli bilimsel makaleleri sıralayan bir asistansın. 
             """;
 
     public static final String ARTICLES_SOURCES = """
             Kullanıcıdan gelen paramtrelere dayanarak (makalenin başlığı)
-            o makaleyi tespit edip o makalede yer alan tüm kaynakları kullanıcının istediği formatta listeleyen bir asistansın
+            o makaleyi tespit edip o makalede yer alan tüm kaynakları kullanıcının istediği formatta
+             listeleyen bir asistansın.
             """;
     public static final String GET_QUOTATION_OF_ARTICLE = """
             Kullanıcıdan gelen parametrelere dayanarak (makalenin başlığı)
-            o makaleyi tespit edip o makalenin alıntılandığı en önemli makaleleri listeleyen bir asistansın
+            o makaleyi tespit edip o makalenin yapılan en önemli atıfları listeleyen bir asistansın.
             """;
 
     public static final String GET_ABSTRACT_OF_ARTICLE = """
@@ -21,7 +22,11 @@ public class Prompts {
             listeleyen bir asistansın: 
             * türkçe dilinde
             * makalenin özet ve sonuç bölümünden yararlanarak
-            * makaleye yapılan alıntıları dikkate alarak
+            * makalede yapılan alıntıları dikkate alarak
+            * makaleye yapılan atıfları dikkate alarak
+            * makalenin literatüre sağladığı katkıyı göze alarak
+            * makalenin çözmeyi vaad edip çözemediği noktalar
+            * makalenin yayınlandığı derginin saygınlığı
             * hem teknik hem de anlamsal açıdan
             """;
 
