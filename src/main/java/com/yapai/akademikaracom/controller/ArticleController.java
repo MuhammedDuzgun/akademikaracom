ac2ea5467113e4213b96560aaecd0a60a8240cdd
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-07-07 18:57:03 +0300

    delete unnecessary methods

--- src/main/java/com/yapai/akademikaracom/controller/ArticleController.java
+++ src/main/java/com/yapai/akademikaracom/controller/ArticleController.java
@@ -1,12 +1,7 @@
 package com.yapai.akademikaracom.controller;
 
-import com.yapai.akademikaracom.request.GetAbstractOfArticleRequest;
-import com.yapai.akademikaracom.request.GetArticlesSourcesRequest;
-import com.yapai.akademikaracom.request.GetQuotationOfArticleRequest;
-import com.yapai.akademikaracom.response.AbstractOfArticleResponse;
 import com.yapai.akademikaracom.response.ArticleResponse;
 import com.yapai.akademikaracom.request.GetArticleRequest;
-import com.yapai.akademikaracom.response.ArticleSourceResponse;
 import com.yapai.akademikaracom.service.ai.ArticleAIService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,28 +22,6 @@ public class ArticleController {
     public ResponseEntity<List<ArticleResponse>> getReleatedArticles(@RequestBody GetArticleRequest request) {
         List<ArticleResponse> response = articleAIService.getArticles(request);
         return ResponseEntity.ok(response);
-    }
-
-
-    @PostMapping("/sources")
-    public ResponseEntity<List<ArticleSourceResponse>> getArticleSources
-            (@RequestBody GetArticlesSourcesRequest request) {
-        List<ArticleSourceResponse> sourcesOfArticle = articleAIService.getSourcesOfArticle(request);
-        return ResponseEntity.ok(sourcesOfArticle);
-    }
-
-    @PostMapping("/quotations")
-    public ResponseEntity<List<ArticleResponse>> getQuotedArticles
-            (@RequestBody GetQuotationOfArticleRequest request) {
-        List<ArticleResponse> quotationOfArticle = articleAIService.getQuotationOfArticle(request);
-        return ResponseEntity.ok(quotationOfArticle);
-    }
-
-    @PostMapping("/abstract")
-    public ResponseEntity<AbstractOfArticleResponse> getAbstractOfArticle
-            (@RequestBody GetAbstractOfArticleRequest request) {
-        AbstractOfArticleResponse abstractOfArticle = articleAIService.getAbstractOfArticle(request);
-        return ResponseEntity.ok(abstractOfArticle);
     }
 
 }
--- src/main/java/com/yapai/akademikaracom/service/ai/ArticleAIService.java
+++ src/main/java/com/yapai/akademikaracom/service/ai/ArticleAIService.java
@@ -1,12 +1,7 @@
 package com.yapai.akademikaracom.service.ai;
 
-import com.yapai.akademikaracom.request.GetAbstractOfArticleRequest;
-import com.yapai.akademikaracom.request.GetArticlesSourcesRequest;
-import com.yapai.akademikaracom.request.GetQuotationOfArticleRequest;
-import com.yapai.akademikaracom.response.AbstractOfArticleResponse;
 import com.yapai.akademikaracom.response.ArticleResponse;
 import com.yapai.akademikaracom.request.GetArticleRequest;
-import com.yapai.akademikaracom.response.ArticleSourceResponse;
 import com.yapai.akademikaracom.utils.Prompts;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.core.ParameterizedTypeReference;
@@ -35,37 +30,4 @@ public class ArticleAIService {
                 .call()
                 .entity(new ParameterizedTypeReference<>() {});
     }
-
-    public List<ArticleSourceResponse> getSourcesOfArticle(GetArticlesSourcesRequest request) {
-        return clientBuilder
-                .defaultSystem(Prompts.ARTICLES_SOURCES)
-                .build()
-                .prompt()
-                .user(u-> u.text("makalenin başlığı : " + request.title() +
-                        "kaynaklarının formatı : " + request.sourceFormat()
-                ))
-                .call()
-                .entity(new ParameterizedTypeReference<>() {});
-    }
-
-    public List<ArticleResponse> getQuotationOfArticle(GetQuotationOfArticleRequest request) {
-        return clientBuilder
-                .defaultSystem(Prompts.GET_QUOTATION_OF_ARTICLE)
-                .build()
-                .prompt()
-                .user(u -> u.text("makalenin başlığı : " + request.articleTitle()))
-                .call()
-                .entity(new ParameterizedTypeReference<>() {});
-    }
-
-    public AbstractOfArticleResponse getAbstractOfArticle(GetAbstractOfArticleRequest request) {
-        return clientBuilder
-                .defaultSystem(Prompts.GET_ABSTRACT_OF_ARTICLE)
-                .build()
-                .prompt()
-                .user(u-> u.text("makalenin başlığı : " + request.title()))
-                .call()
-                .entity(new ParameterizedTypeReference<>() {});
-    }
-
 }
