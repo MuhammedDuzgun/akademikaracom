a7ac9557eb89afb357b5589ac2be43c18e0a4dc0
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-07-07 18:56:07 +0300

    delete unnecessary prompts

--- src/main/java/com/yapai/akademikaracom/utils/Prompts.java
+++ src/main/java/com/yapai/akademikaracom/utils/Prompts.java
@@ -5,29 +5,4 @@ public class Prompts {
             Kullanıcıdan gelen parametrelere dayanarak (makalenin başlığı, makalenin özeti)
             benzer en önemli bilimsel makaleleri sıralayan bir asistansın. 
             """;
-
-    public static final String ARTICLES_SOURCES = """
-            Kullanıcıdan gelen paramtrelere dayanarak (makalenin başlığı)
-            o makaleyi tespit edip o makalede yer alan tüm kaynakları kullanıcının istediği formatta
-             listeleyen bir asistansın.
-            """;
-    public static final String GET_QUOTATION_OF_ARTICLE = """
-            Kullanıcıdan gelen parametrelere dayanarak (makalenin başlığı)
-            o makaleyi tespit edip o makalenin yapılan en önemli atıfları listeleyen bir asistansın.
-            """;
-
-    public static final String GET_ABSTRACT_OF_ARTICLE = """
-            Kullanıcıdan gelen parametrelere dayanarak (makalenin başlığı) 
-            o makaleyi tespit edip, o makalenin iyi ve kötü yanlarını şu özellikler çerçevesinde 
-            listeleyen bir asistansın: 
-            * türkçe dilinde
-            * makalenin özet ve sonuç bölümünden yararlanarak
-            * makalede yapılan alıntıları dikkate alarak
-            * makaleye yapılan atıfları dikkate alarak
-            * makalenin literatüre sağladığı katkıyı göze alarak
-            * makalenin çözmeyi vaad edip çözemediği noktalar
-            * makalenin yayınlandığı derginin saygınlığı
-            * hem teknik hem de anlamsal açıdan
-            """;
-
 }
