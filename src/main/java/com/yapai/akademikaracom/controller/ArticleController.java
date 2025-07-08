1d307ccfebc49ac9e28cbc13787c746f8d9d5b71
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-07-08 19:01:48 +0300

    delete unnecessary methods

--- src/main/java/com/yapai/akademikaracom/controller/ArticleController.java
+++ src/main/java/com/yapai/akademikaracom/controller/ArticleController.java
@@ -1,15 +1,12 @@
 package com.yapai.akademikaracom.controller;
 
 import com.yapai.akademikaracom.response.ArticleAnalyze;
-import com.yapai.akademikaracom.response.ArticleResponse;
-import com.yapai.akademikaracom.request.GetArticleRequest;
 import com.yapai.akademikaracom.service.ai.ArticleAIService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/articles")
@@ -19,12 +16,6 @@ public class ArticleController {
 
     public ArticleController(ArticleAIService articleAIService) {
         this.articleAIService = articleAIService;
-    }
-
-    @PostMapping
-    public ResponseEntity<List<ArticleResponse>> getReleatedArticles(@RequestBody GetArticleRequest request) {
-        List<ArticleResponse> response = articleAIService.getArticles(request);
-        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/analyze-article")
--- src/main/java/com/yapai/akademikaracom/service/ai/ArticleAIService.java
+++ src/main/java/com/yapai/akademikaracom/service/ai/ArticleAIService.java
@@ -1,8 +1,6 @@
 package com.yapai.akademikaracom.service.ai;
 
 import com.yapai.akademikaracom.response.ArticleAnalyze;
-import com.yapai.akademikaracom.response.ArticleResponse;
-import com.yapai.akademikaracom.request.GetArticleRequest;
 import com.yapai.akademikaracom.utils.Prompts;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -14,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 @Service
 public class ArticleAIService {
@@ -23,18 +20,6 @@ public class ArticleAIService {
 
     public ArticleAIService(ChatClient.Builder clientBuilder) {
         this.clientBuilder = clientBuilder;
-    }
-
-    public List<ArticleResponse> getArticles(GetArticleRequest request) {
-        return clientBuilder
-                .defaultSystem(Prompts.GET_SIMILAR_ARTICLES)
-                .build()
-                .prompt()
-                .user(u -> u.text("makalenin başlığı : " + request.title() +
-                        "makalenin özeti : " + request.articleAbstract()
-                ))
-                .call()
-                .entity(new ParameterizedTypeReference<>() {});
     }
 
     public ArticleAnalyze analyzePdfArticle(MultipartFile file) throws IOException {
