a77862ab211b0833b7813fe8e65227dfbcd6b4fd
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-07-09 03:45:43 +0300

    add SecurityConfig

---
+++ src/main/java/com/yapai/akademikaracom/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.yapai.akademikaracom.config;
+
+import com.yapai.akademikaracom.service.crud.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import java.io.IOException;
+
+@Configuration
+public class SecurityConfig {
+
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return
+                httpSecurity
+                        .cors(AbstractHttpConfigurer::disable)
+                        .csrf(AbstractHttpConfigurer::disable)
+                        .authorizeHttpRequests(registry -> {
+                            registry.requestMatchers("/",
+                                    "/index.html",
+                                    "/login.html",
+                                    "/style.css",
+                                    "/main.js").permitAll();
+                            registry.requestMatchers("/login").permitAll();
+                            registry.anyRequest().authenticated();
+                        })
+                        .oauth2Login(oauth2login -> {
+                            oauth2login.loginPage("/login");
+                            oauth2login.successHandler(new AuthenticationSuccessHandler() {
+                                @Override
+                                public void onAuthenticationSuccess(HttpServletRequest request,
+                                                                    HttpServletResponse response,
+                                                                    Authentication authentication)
+                                        throws IOException, ServletException {
+                                    userService.addUser(authentication);
+                                    authentication.getCredentials();
+                                    response.sendRedirect("/");
+                                }
+                            });
+                        })
+                        .exceptionHandling(exception -> exception
+                                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                        .build();
+    }
+
+}
