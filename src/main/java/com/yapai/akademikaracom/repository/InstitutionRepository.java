ef7032366eef4562745dcfda88da477bb7f2cddf
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-07-20 03:20:15 +0300

    add InstitutionRepository, WorkRepository

---
+++ src/main/java/com/yapai/akademikaracom/repository/InstitutionRepository.java
@@ -0,0 +1,7 @@
+package com.yapai.akademikaracom.repository;
+
+import com.yapai.akademikaracom.entity.Institution;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InstitutionRepository extends JpaRepository<Institution, Long> {
+}
---
+++ src/main/java/com/yapai/akademikaracom/repository/WorkRepository.java
@@ -0,0 +1,7 @@
+package com.yapai.akademikaracom.repository;
+
+import com.yapai.akademikaracom.entity.Work;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkRepository extends JpaRepository<Work, Long> {
+}
